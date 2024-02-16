1a4f1572c319995a5961bcec723bf497683c5694
Derek Schwantner <derek@Dereks-Mac-mini.local>
2024-02-15 21:50:14 -0500

    boilerplate is working

--- src/app/context/AuthProvider.js
+++ src/app/context/AuthProvider.js
@@ -1,5 +1,7 @@
 'use client'
 
+// this is only necessary for the client components because the server components are already wrapped in a session provider
+
 import { SessionProvider } from 'next-auth/react'
 
 export default function AuthProvider({ children }) {
